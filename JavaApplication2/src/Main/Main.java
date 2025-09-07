5d4cefa57336edc8a848231157c8b9c6106dbce8
Luis Guilherme de Almeida Costa <failho42@gmail.com>
2025-09-06 22:13:44 -0300

    Adicionando login

--- JavaApplication2/src/Main/Main.java
+++ JavaApplication2/src/Main/Main.java
@@ -2,19 +2,19 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package javaapplication2;
+package Main;
 
 /**
  *
  * @author tung tung tung sahur
  */
-public class JavaApplication2 {
+public class Main {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
     
 }
